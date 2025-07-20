446c2cdc366ddfd4efcb3d0c3ba05a0c066308c8
Abhinav Singh <heysinghabhinav@gmail.com>
2025-07-20 17:18:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ print("Flask app setup complete!")
 
 if __name__ == "__main__":
     load_dotenv()
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
